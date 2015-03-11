9976dc0bfabf026611f36c0e1651084d68c847e3
Valentyn Shybanov <vshybanov@twinfield.com>
2015-03-11 18:26:32 +0200

    Added other types support

--- parse.js
+++ parse.js
@@ -2,7 +2,9 @@ var util = require('util');
 var xml = require("node-xml");
 
 
-module.exports = function(filename, caller_cb, options) {
+function Parser() {}
+Parser.prototype.parse = function(filename, caller_cb, options) {
+    var self = this;
     if (options) {
         var temp_cb = caller_cb;
         caller_cb = options;
@@ -14,9 +16,10 @@ module.exports = function(filename, caller_cb, options) {
     
     var parser = new xml.SaxParser(function(cb) {
         cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+	
+	    var shouldCheck = self.shoudInclude(elem, attrs, prefix, uri, namespaces);
 
-            if (elem != 'element') return;
-            if (attrs[0][0] != 'name') return;        
+	    if (!shouldCheck) return;
 
             if (!rootVisited && options && options.skipRoot) { 
                 rootVisited = true;
@@ -25,7 +28,7 @@ module.exports = function(filename, caller_cb, options) {
 
             rootVisited = true;            
 
-            var name = attrs[0][1];            
+            var name = self.getName(elem, attrs, prefix, uri, namespaces);
             path.push(name);
 
             var p = path.join('/');
@@ -34,7 +37,7 @@ module.exports = function(filename, caller_cb, options) {
             }
         });
         cb.onEndElementNS(function(elem, prefix, uri) {            
-            if (elem != 'element') return;
+            if (!self.shouldGoBack(elem, prefix, uri,options)) return;
             path.pop();
         });
         cb.onError(function(msg) {
@@ -48,3 +51,33 @@ module.exports = function(filename, caller_cb, options) {
     return parser.parseFile(filename);
 
 }
+
+xsdParser = new Parser();
+
+xsdParser.shoudInclude = function(elem, attrs) {
+            return elem == 'element'&& attrs[0][0] == 'name';
+}
+xsdParser.shouldGoBack = function(elem, attrs) {
+            return elem == 'element';
+}
+
+xsdParser.getName = function(elem, attrs) {
+	    return attrs[0][1];
+}
+
+xmlParser = new Parser();
+
+xmlParser.shoudInclude = function(elem, attrs) {
+	return true;
+}
+xmlParser.shouldGoBack = function(elem, attrs) {
+	return true;
+}
+
+xmlParser.getName = function(elem, attrs) {
+	return elem;
+}
+
+
+module.exports.parseXsd = xsdParser.parse.bind(xsdParser);
+module.exports.parseXml = xmlParser.parse.bind(xmlParser);
